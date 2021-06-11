df8e8ee878a2479e23ffd4fe046fe771331ea912
globalsysroberto <75792848+rscarlosjunior@users.noreply.github.com>
2021-06-11 01:42:19 -0300

    [feature] configure home

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container pb-20">
+    <!-- <Characters />
+    <Episodes /> -->
+  </div>
+</template>
+
+<script>
+// import Character from '@/components/Character'
+// import Episodes from '@/components/Episodes'
+
+export default {
+  name: 'Home',
+  metaInfo: {
+  htmlAttrs: {
+    lang: 'pt-BR'
+  },
+  title: 'Rick and More Wikipedia | Search your spacial information here.',
+  meta: [{
+      name: 'descrition',
+      content: 'Episodes,characteres informations and more.'
+    }],
+  },
+  components: {
+    // Episodes,
+    // Character
+  }
+}
+</script>
