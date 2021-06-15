873143f9fd028d1fddf9744981bc00457ae499c4
globalsysroberto <rs.carlosjunior@gmail.com>
2021-06-15 01:58:52 -0300

    [fix] fix padding

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container banner pt-4">
+  <div class="container banner pt-1">
     <a href="#" class="banner__link">
       <picture>
         <source srcset="../assets/img/banner.png" media="(max-width: 992px)" class="banner__img">
