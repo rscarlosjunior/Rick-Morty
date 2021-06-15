97e531bb153f139d5c07012c2a87c52891a205de
globalsysroberto <rs.carlosjunior@gmail.com>
2021-06-15 02:03:43 -0300

    [feature] creating step buttons components

---
+++ src/components/stepButtons.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="step__buttons ">
+      <span @click="prevPage()" class="previous round" :class="{'disabled': disable }">&#8249;</span>
+      <span @click="nextPage()" class="next round">&#8250;</span>
+    </div>
+</template>
+<script>
+export default {
+    name: "StepButton",
+    props:{
+        disable:{type:Boolean}
+    },
+    methods:{
+        nextPage(){
+            this.$emit('nextPage')
+        },
+        prevPage(){
+            this.$emit('prevPage')
+        }
+    }
+}
+</script>
