835c8317e0e9e77ba6e66c4a97e9aab548713761
globalsysroberto <rs.carlosjunior@gmail.com>
2021-06-15 02:01:56 -0300

    [fix] remove episodes methods and include search and stepbuttons

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,55 +1,50 @@
 <template>
   <section class="container pt-4">
-  <p class="character__title"> Rick and Morty Character List </p>
-  <div class="character__search-box">
-    <input type="text" v-model="character_name" class="character__search" placeholder="The name is here">
-    <button class="character__search-btn desktop'">
-      <span @click="getCharByName(character_name)">Find your Character</span>
-    </button>
-  </div>
-  <div class="characters">
-    <div v-for="char in getCharacters.results" :key="char.id" class="character__item">
-      <div class="character__box">
-        <div class="character__img-box">
-          <img class="character__img" :src="char.image" />
-        </div>
-        <div class="character__info">
-          <h3 class="character__name">{{ char.name }}</h3>
-          <div class="information-box">
-            <div class="all-info">
-              <span class="character__customer"> <span class="character__description">Status:</span> {{ char.status }}</span>
-              <span class="character__customer"> <span class="character__description">Species:</span> {{ char.species }}</span>
-              <span class="character__customer"> <span class="character__description">Origin:</span> {{ char.origin.name }}</span>
-              <span class="character__customer"> <span class="character__description">Total Episodes:</span> {{ char.episode.length }}</span>
+    <search @iniciatedSearch="getCharByName" title="Rick and Morty Character List" placeholder="The name is here" finder="Find your Character"/>
+    <div class="characters">
+      <div v-for="char in getCharacters.results" :key="char.id" class="character__item">
+        <div class="character__box">
+          <div class="character__img-box">
+            <img class="character__img" :src="char.image" />
+          </div>
+          <div class="character__info">
+            <h3 class="character__name">{{ char.name }}</h3>
+            <div class="information-box">
+              <div class="all-info">
+                <span class="character__customer"> <span class="character__description">Status:</span> {{ char.status }}</span>
+                <span class="character__customer"> <span class="character__description">Species:</span> {{ char.species }}</span>
+                <span class="character__customer"> <span class="character__description">Origin:</span> {{ char.origin.name }}</span>
+                <span class="character__customer"> <span class="character__description">Total Episodes:</span> {{ char.episode.length }}</span>
+              </div>
+              <button
+                :class="['btn character__btn desktop',{'btn--green': char.name,},]"
+              >
+                <span @click="getInfo(char.name)"> More information</span>
+              </button>
             </div>
-            <button
-              :class="['btn character__btn desktop',{'btn--green': char.name,},]"
-            >
-              <span @click="getInfo(char.name)"> More information</span>
-            </button>
           </div>
         </div>
+        <button
+          :class="['btn character__btn mobile',{'btn--green': char.name,},]"
+        >
+          <span @click="getInfo(char.name)"> More information</span>
+        </button>
       </div>
-      <button
-        :class="['btn character__btn mobile',{'btn--green': char.name,},]"
-      >
-        <span @click="getInfo(char.name)"> More information</span>
-      </button>
     </div>
-  </div>
-  <div class="character__buttons">
-    <span @click="prevPage()" class="previous round" :class="{'disabled': disable }">&#8249;</span>
-    <span @click="nextPage()" class="next round">&#8250;</span>
-  </div>
+    <step-buttons @nextPage="nextPage" @prevPage="prevPage" :disable="disable"/>
   </section>
 </template>
 
 <script>
 import store from "@/store"
-import { getPaginationCharacters, setCharacters, setEpisodes, getCharactersByName } from '@/assets/js/utils/apiCaller'
+import Search from './Search'
+import StepButtons from './stepButtons'
+import { getPaginationCharacters, setCharacters, getCharactersByName } from '@/assets/js/utils/apiCaller'
 
 export default {
-  data(){
+  components: { Search, StepButtons },
+  name: "Characters",
+  data() {
     return {
       page: 1,
       character_name: ''
@@ -65,7 +60,6 @@ export default {
   },
   created() {
     setCharacters()
-    setEpisodes()
   },
   methods: {
     getInfo(character) {
