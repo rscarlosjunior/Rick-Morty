fcf7810e3c86ad00dc76e1a7e67d7ff6d9b660e7
globalsysroberto <rs.carlosjunior@gmail.com>
2021-06-15 02:05:09 -0300

    [feature] include pagination episodes.

--- src/services/API.js
+++ src/services/API.js
@@ -8,5 +8,6 @@ export const api = {
     getCharacters: () => axiosInstance.get('/character'),
     getPaginationCharacters: (page) => axiosInstance.get(`/character/?page=${page}`),
     getCharactersByName: (name) => axiosInstance.get(`/character/?name=${name}`),
-    getEpisodes: () => axiosInstance.get('/episode')
+    getEpisodes: (number) => axiosInstance.get(`/episode/${number ? number : ''}`),
+    getPaginationEpisodes: (page) => axiosInstance.get(`/episode/?page=${page}`),
 }
